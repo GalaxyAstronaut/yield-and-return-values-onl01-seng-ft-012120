4431ba75de0807f8b4198308ec1c937168782f66
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 17:46:24 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,8 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-  collection << yield(array[i])
-  i += 1
+    collection << yield(array[i])
+    i += 1
   end
   collection
 end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
